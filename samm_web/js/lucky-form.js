80f582a92b8e53f3815eeade955858f6f5d5dbcb
Luc Avronsart <ladevweb@gmail.com>
2019-05-18 16:50:46 +0200

    Create plugin form

---
+++ samm_web/js/lucky-form.js
@@ -0,0 +1,41 @@
+function bindMaterialInput() {
+	$(".form_field input").bind({
+		"blur" : function () {
+			if ($(this).val() !== "") {
+				$(this).prev().addClass("active");
+			} else {
+				$(this).prev().removeClass("active");
+			}
+		}
+	});
+}
+
+function validateForm(form) {
+	if (form.find($("input")).length === form.find($("input.valid")).length) {
+		form.find("button").removeClass("disabled");
+	} else {
+		form.find("button").addClass("disabled");
+	}
+}
+
+function validEmailAddress(email) {
+	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  	return regex.test(email);
+}
+
+function isTooShortString(string, length) {
+	if (string.length < length) {
+		return true;
+	} 
+	return false;
+}
+
+function showInputError(input, message) {
+	input.next().text(message).fadeIn();
+	input.removeClass("")	
+}
+
+function validInput(input) {
+	input.next().fadeOut();
+	input.addClass("valid");
+}
--- samm_web/js/samm.js
+++ samm_web/js/samm.js
@@ -62,28 +62,6 @@ function logOut() {
 	goToView($(".login_view"));
 }
 
-function validEmailAddress(email) {
-	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  	return regex.test(email);
-}
-
-function isTooShortString(string, length) {
-	if (string.length < length) {
-		return true;
-	} 
-	return false;
-}
-
-function showInputError(input, message) {
-	input.next().text(message).fadeIn();
-	input.removeClass("")	
-}
-
-function validInput(input) {
-	input.next().fadeOut();
-	input.addClass("valid");
-}
-
 function goToView(view) {
 	$(".view").hide();
 	view.fadeIn();
@@ -605,18 +583,6 @@ function bindHomeView() {
 		}
 	});
 	
-}
-
-function bindMaterialInput() {
-	$(".form_field input").bind({
-		"blur" : function () {
-			if ($(this).val() !== "") {
-				$(this).prev().addClass("active");
-			} else {
-				$(this).prev().removeClass("active");
-			}
-		}
-	});
 }
 
 
