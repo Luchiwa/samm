d3d6603bc81fe636c67683d60ccac62831202d43
Luc Avronsart <ladevweb@gmail.com>
2019-05-29 14:15:44 +0200

    SAMM WEB : When user add taken, his success are invalid, user lose success points

--- samm_web/js/samm-add-taken.js
+++ samm_web/js/samm-add-taken.js
@@ -24,9 +24,28 @@ function bindAddTakenForm() {
 					if (jqXHR.status === 201) {
 						var taken = jqXHR.responseJSON;
 						localStorage.setItem(getLocalStorage("taken:addiction_id") + ":reset_date", taken.creation_date);
-						popin("C'est dommage, essayer de faire mieux.", function () {
-							window.location.href="index.html";
-						});
+						$.ajax({
+							url : apiBaseUrl + "user_success/disable_validity.php",
+							method : "POST",
+							contentType: "application/json",
+							dataType: "json",
+							data : JSON.stringify({
+								"addiction" : getLocalStorage("taken:addiction_id"),
+								"user" : getLocalStorage("user.id")
+							}),
+							complete: function (jqXHR) {
+								if (jqXHR.status === 200) {
+									localStorage.setItem("user.score", jqXHR.responseJSON.score);
+									popin("C'est dommage, tu étais bien parti, tu as invalidé des succés, ton score est de "+jqXHR.responseJSON.score+". Essayes de faire mieux maintenant.", function () {
+										window.location.href="index.html";
+									});
+								} else {
+									popin("C'est dommage, essaies de faire mieux maintenant.", function () {
+										window.location.href="index.html";
+									});
+								}								
+							}
+						});						
 					} else {
 						popin("Service indisponible", function () {
 							window.location.href="index.html";
